41ad99aa5ddeabfe2c5a5def3d6d390bdd1a6f1c
Tran Lam Tu <justin.tu@Trans-MacBook-Air.local>
2022-02-05 12:00:09 -0500

    lift state up from LightSwitch to App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import TextRepeaterButton from "./components/TextRepeaterButton";
 import './App.css';
 
 function App() {
+
+  const [light, setLight] = useState("off")
+
+
   return (
     <div className="App">
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton/>
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
         <TextRepeaterButton />
       </section>
     </div>
